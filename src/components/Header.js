c4928d8a74f139aaa60ea9fff5c9f36acfb1c96f
Attah David <jocheattahdavid@gmail.com>
2020-09-02 11:14:56 +0100

    setup react router

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Header.css';
+
+function Header() {
+  return (
+    <nav className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/download/61245"
+        alt= "Amazon Logo"
+      />
+      {/* search box */}
+      {/* 3 liknks */}
+    </nav>
+  );
+}
+
+export default Header;
