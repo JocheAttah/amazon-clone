af1164b24d98065a376a068a9845615dcfe33807
Attah David <jocheattahdavid@gmail.com>
2020-09-16 23:27:32 +0100

    fixed images

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,9 +14,9 @@ function Home() {
         <Product
           id={1234556}
           title="Sony WH-1000XM4 Wireless Industry Leading Noise Canceling Overhead Headphones with Mic for phone-call and Alexa voice control, Black"
-          price={348.00}
+          price={348.0}
           rating={5}
-          image="https://m.media-amazon.com/images/I/71o8Q5XJS5L._AC_UY218_.jpg"
+          image="https://www.sony.com/image/5d02da5df552836db894cead8a68f5f3?fmt=png-alpha&wid=440"
         />
         <Product
           id={55648839}
@@ -24,7 +24,7 @@ function Home() {
 Sony Noise Cancelling Headphones WHCH710N: Wireless Bluetooth Over the Ear Headset with Mic for phone-call, Blue (Amazon Exclusive) (WHCH710N/L)"
           price={98.25}
           rating={4}
-          image="https://m.media-amazon.com/images/I/511G0DPMuQL._AC_UY218_.jpg"
+          image="https://store.sony.com.au/dw/image/v2/ABBC_PRD/on/demandware.static/-/Sites-sony-master-catalog/default/dw164777fa/images/WHCH710NL/WHCH710NL.png"
         />
       </div>
       <div className="home__row">
@@ -33,21 +33,21 @@ Sony Noise Cancelling Headphones WHCH710N: Wireless Bluetooth Over the Ear Heads
           title="Bose SoundLink Color Bluetooth Speaker II - Soft Black"
           price={129.99}
           rating={5}
-          image="https://m.media-amazon.com/images/I/71Xg3PvaiQL._AC_UY218_.jpg"
+          image="https://assets.bose.com/content/dam/Bose_DAM/Web/consumer_electronics/global/products/speakers/soundlink_color_ii/product_silo_images/soundlink_color_ii_black_EC.psd/_jcr_content/renditions/cq5dam.web.320.320.png"
         />
         <Product
           id={75840383384}
           title="Sonos Move - Battery-powered Smart Speaker, Wi-Fi and Bluetooth with Alexa built-in - Black​​​​​​​"
           price={191.96}
           rating={4}
-          image="https://m.media-amazon.com/images/I/91Ckx1BIckL._AC_UY218_.jpg"
+          image="https://crdms.images.consumerreports.org/f_auto,w_600/prod/products/cr/models/400076-smart-speakers-sonos-move-10009083.png"
         />
         <Product
           id={24638404440}
           title="Bose SoundLink Revolve, Portable Bluetooth Speaker (with 360 Wireless Surround Sound), Triple Black"
           price={121.96}
           rating={3}
-          image="https://m.media-amazon.com/images/I/81ipKYwWZBL._AC_UY218_.jpg"
+          image="https://dalooni.com/image/cache/catalog/BOSE/739523-5110-1-550x550w.png"
         />
       </div>
       <div className="home__row">
@@ -57,9 +57,8 @@ Sony Noise Cancelling Headphones WHCH710N: Wireless Bluetooth Over the Ear Heads
 Sony X900H 65 Inch TV: 4K Ultra HD Smart LED TV with HDR and Alexa Compatibility - 2020 Model"
           price={1551.96}
           rating={4}
-          image="https://images-na.ssl-images-amazon.com/images/I/612U-Yeox0L._AC_SL1080_.jpg"
+          image="https://lh3.googleusercontent.com/proxy/lzPUIFOAHZSgR1KQ9f_kfhT1MkkwPC5mXi4rlfTt-eRDnTAGZzQuZCcmYLtVKQEIyuygexW4LnHnxqWZhmvguuIUXfIIHP9f3tDDJrNT4qRyJWVhpGvWyVFwqBgZvH3Mu7-xBSJRuNP7WF7b4q6Abio"
         />
-        
       </div>
     </div>
   );
--- src/components/axios.js
+++ src/components/axios.js
@@ -2,8 +2,12 @@ import axios from "axios";
 
 
 const instance = axios.create({
-   baseURL:"http://localhost:5001/clone-8a53f/us-central1/api" //the API url cloud function
+   baseURL: 'https://us-central1-clone-8a53f.cloudfunctions.net/api'
+   
+//    "http://localhost:5001/clone-8a53f/us-central1/api" //the API url cloud function
 });
 
 
 export default instance;
+
+// https://us-central1-clone-8a53f.cloudfunctions.net/api
